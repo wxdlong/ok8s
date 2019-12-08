1d6731caa9537b450a065d6ae99c4ae9ddd20899
wxdlong <wxdlong@qq.com>
2019-12-08 22:48:34 +0800

    ok8s.sh init

--- ok8s.sh
+++ ok8s.sh
@@ -4,12 +4,30 @@ set -e
 
 NodeName=ok8s
 
-
-function preflight(){
-    
+function preflight() {
+   echo "prefilt"
 }
 
-function initK8s(){
+function initK8s() {
     kubeadm init --node-name=${NodeName} --v=7
 
 }
+
+function enableServices(){
+    #/usr/lib/systemd/system
+    
+}
+
+function logI(){
+    echo -e "[INFO] \033[33m  ${FUNCNAME[1]}\033[0m : $1"
+}
+function flushIptables() {
+    logI "starting"
+    iptables -F && iptables -X &&
+        iptables -F -t nat && iptables -X -t nat &&
+        iptables -F -t raw && iptables -X -t raw &&
+        iptables -F -t mangle && iptables -X -t mangle
+
+}
+
+$@
